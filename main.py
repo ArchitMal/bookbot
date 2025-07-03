718e1be9186ef13f4c48d098dfe9018a648bdbc8
ArchitMal <archit.malyala@gmail.com>
2025-07-03 13:44:34 -0400

    prints out frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+
+if __name__ == "__main__":
+    main()
