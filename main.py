f8a4846ff0a72f0f0cc554a11af69e4dd19ee425
ArchitMal <archit.malyala@gmail.com>
2025-07-04 22:38:41 -0400

    added sys

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from stats import get_book_text
+from stats import character_counter
+from stats import sorter
+from stats import sort_on
+import sys
 
 def main():
-    number_of_words = get_book_text("./books/frankenstein.txt")
-    print(f"{number_of_words} words found in the document")
-
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    number_of_words = get_book_text(book_path)
+    char_count = character_counter(book_path)
+    final = sorter(char_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {number_of_words} total words")
+    print("--------- Character Count -------")
+    for character in final:
+        print(f"{character['char']}: {character['num']}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,3 +2,28 @@ def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return len(file_contents.split())
+
+def character_counter(path):
+    with open(path) as f:
+        file_contents = f.read()
+    
+    final = {}
+    for char in file_contents:
+        if char.lower() in final:
+            final[char.lower()] += 1
+        else:
+            final[char.lower()] = 1
+    
+    return final
+
+def sort_on(items):
+    return items["num"]
+
+def sorter(values):
+    result = []
+    for key, value in values.items():
+        if key.isalpha():
+            temp = {"char":key.lower(), "num":value}
+            result.append(temp)
+    result.sort(reverse=True, key=sort_on)
+    return result
