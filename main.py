bf295ba057e72ef912786bf913e62241d5183830
ArchitMal <archit.malyala@gmail.com>
2025-07-04 21:47:54 -0400

    Added stats.py and moved function

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def get_book_text(path):
-    with open(path) as f:
-        file_contents = f.read()
-    return len(file_contents.split())
+from stats import get_book_text
 
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    number_of_words = get_book_text("./books/frankenstein.txt")
+    print(f"{number_of_words} words found in the document")
 
 
 if __name__ == "__main__":
